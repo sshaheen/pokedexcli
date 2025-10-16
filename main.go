4ff1e19cca0d5fc91580b25d7781fc75da6b0126
Salem Shaheen <salem.shaheen894@gmail.com>
2025-10-16 15:12:33 -0400

    Add Map Command

--- main.go
+++ main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
+	"io"
+	"net/http"
 	"os"
 	"strings"
 )
@@ -10,11 +13,26 @@ import (
 type cliCommand struct {
 	name        string
 	description string
-	callback    func() error
+	callback    func(c *config) error
+}
+
+type config struct {
+	Next     string
+	Previous string
+}
+
+type mapData struct {
+	Results []location
+}
+
+type location struct {
+	Name string
+	Url  string
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
+	c := &config{}
 
 	command_map := map[string]cliCommand{
 		"exit": {
@@ -27,6 +45,11 @@ func main() {
 			description: "Provide user with usage info",
 			callback:    commandHelp,
 		},
+		"map": {
+			name:        "map",
+			description: "Provide list of next 20 locations in Pokemon world",
+			callback:    commandMap,
+		},
 	}
 
 	for {
@@ -37,7 +60,7 @@ func main() {
 		command := cleaned_input[0]
 		c_str, ok := command_map[command]
 		if ok {
-			c_str.callback()
+			c_str.callback(c)
 		} else {
 			fmt.Println("Unknown command")
 		}
@@ -56,17 +79,48 @@ func cleanInput(text string) []string {
 	return result
 }
 
-func commandExit() error {
+func commandExit(c *config) error {
 	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error {
+func commandHelp(c *config) error {
 	fmt.Println("Welcome to the Pokedex!")
 	fmt.Println("Usage:")
 	fmt.Printf("\n\n")
 	fmt.Println("help: Displays a help message")
 	fmt.Println("exit: Exit the Pokedex")
+	fmt.Println("map: Displays list of next 20 locations in Pokemon world")
+	return nil
+}
+
+func commandMap(c *config) error {
+	res, err := http.Get("https://pokeapi.co/api/v2/location-area")
+
+	if err != nil {
+		return err
+	}
+
+	body, err := io.ReadAll(res.Body)
+
+	if err != nil {
+		return err
+	}
+
+	res.Body.Close()
+
+	var map_data mapData
+
+	err = json.Unmarshal(body, &map_data)
+
+	if err != nil {
+		return err
+	}
+
+	for _, item := range map_data.Results {
+		fmt.Printf("Name: %s\n", item.Name)
+	}
+
 	return nil
 }
